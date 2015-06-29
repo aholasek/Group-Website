cccb6816d94f42a5b43113ff1368ce665f48ab70
aholasek <student8@js-c02mxrmag083.punahou.edu>
2015-06-29 13:59:02 -1000

    app.js commit 2

---
+++ app.js
@@ -0,0 +1,51 @@
+
+
+function computerThink(){
+	computerChoice = Math.floor(Math.random()*3);
+	if (computerChoice) === 1){
+	computerChoice = "rock";
+	}
+	else if(computerChoice === 2){
+		computerChoice = "paper";
+	}
+	else{
+		computerChoice = "scissors";
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function compare(choice1, choice2) {
+	if (choice1 === choice2)
+
+
+}
